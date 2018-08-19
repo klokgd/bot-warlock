3430cbdd01c1af7178b466bce727d800ec111922
mariupol <jokzik@gmail.com>
2018-08-19 18:19:31 +0300

    Добавил config.py без токена

---
+++ config.py
@@ -0,0 +1,11 @@
+token = ''
+database_name = 'warlock.db'
+shelve_name = 'shelve.db'
+REQUEST_KWARGS={
+    'proxy_url': 'http://socks5.svoi-an.us:1080/',
+    # Optional, if you need authentication:
+    'urllib3_proxy_kwargs': {
+        'username': 'privet',
+        'password': 'roskomnadzor',
+    }
+}
