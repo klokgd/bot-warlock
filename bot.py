a85ea033da62e5ea033678b9b867a629a13f7bdc
mariupol <jokzik@gmail.com>
2018-08-19 17:16:59 +0300

    Бот начал отвечать, теперь нужно чтоб отвечал в группах

--- bot.py
+++ bot.py
@@ -5,14 +5,29 @@ from sqlighter import sqlighter
 import os
 import time
 import utils
-from telebot import types
+from telebot import types, apihelper
+import telegram
+from telegram.ext import Updater
+
+apihelper.proxy = {
+    'http': 'socks5://127.0.0.1:9150',
+    'https': 'socks5://127.0.0.1:9150'
+}
+
+
 bot = telebot.TeleBot(config.token)
 
-@bot.message_handler(func=lambda message: True, regexp="([Вв]ар[лок|луша|лоша]|[WwVv]arlo[ck|k]|[Вв]хуриндар)")
+#get_Updates(offset, limit, timeout):
+
+restricted_messages = ["Варлок", "варлок", "Вхуриндар", "угнетать", "угнетают", "либерал", "либерализм", "варлуша", "варлоша"]
+
+@bot.message_handler(regexp="([Вв]ар[лок|луша|лоша]|[WwVv]arlo[ck|k]|[Вв]хуриндар)")
+
 def get_answer(message):
+	to_chat_id = '-237829042'
 	db_worker = sqlighter(config.database_name)
 	row = db_worker.select_single(random.randint(1, utils.get_rows_count()))
-	bot.forward_message(to_chat_id, from_chat_id, row[0])
+	bot.reply_to(message, row[0].format(message.text))
 	db_worker.close()
 
 if __name__ == '__main__':
