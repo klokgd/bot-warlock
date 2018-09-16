1ce801721048e310468a67282dff80d40f9d896a
mariupol <jokzik@gmail.com>
2018-09-16 20:10:14 +0300

    Сделал ответ на реплей, код стал лучше

--- bot.py
+++ bot.py
@@ -4,26 +4,35 @@ import random
 from sqlighter import sqlighter
 import utils
 from telebot import types, apihelper
-
+import time
+import re
 
 apihelper.proxy = {
     'http': 'socks5://127.0.0.1:9150',
     'https': 'socks5://127.0.0.1:9150'
 }
 
+bot = telebot.TeleBot(config.token, skip_pending=True)
 
-bot = telebot.TeleBot(config.token)
+reg = "(^| )[Вв]ар|лок|луша|лоша|]|(^| )[WwVv]arlo|(^| )[Ввх]уриндар|(^| )[Хх]ейтер|(^| )[Рр]абота|(^| )[Пп]рактика|(^| )[Мм]аг|(^| )[Уу]гнета|(^| )([Лл]иберал)|(^| )[Вв]уки|(^| )[Рр]епорт|(^| )срач|(^| )патворкинг|(^| )[Рр]усал|(^| )[Фф]еи"
 
-#get_Updates(offset, limit, timeout):
 
-#restricted_messages = ["Варлок", "варлок", "Вхуриндар", "вхуриндар", "хейтер", "хейтеры", "работает", "Работает!", "практика", "маг", "угнетать", "угнетают", "либерал", "либерализм", "варлуша", "варлоша"]
-
-@bot.message_handler(regexp="([Вв]ар|лок|луша|лоша|]|[WwVv]arlo[ck|k]|[Вв]хуриндар|[Хх]ейтер\D{1,6}|[Рр]абота\D{1,6}|[Пп]рактика|[Мм]аг\D{1,6}|[Уу]гнета\D{1,6}|([Лл]иберал\D{1,6})|[Вв]уки|[Рр]епорт\D{1,6}|срач\D{1,6}|патворкинг)")
+@bot.message_handler(content_types="text")
 def get_answer(message):
-	db_worker = sqlighter(config.database_name)
-	row = db_worker.select_single(random.randint(1, utils.get_rows_count()))
-	bot.reply_to(message, row[0].format(message.text))
-	db_worker.close()
+    db_worker = sqlighter(config.database_name)
+    row = db_worker.select_single(random.randint(1, utils.count_rows()))
+    match = re.search(reg, message.text)
+    if match:
+        # time.sleep(random.randint(15, 60))
+        bot.reply_to(message, row[0].format(message.text))
+    elif message.reply_to_message.from_user.id == bot.get_me().id:
+    # time.sleep(random.randint(15, 60))
+       bot.reply_to(message, row[0].format(message.text))
+
+    # except:
+    #	time.sleep(10)
+
+    db_worker.close()
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -12,13 +12,7 @@ def count_rows():
     rowsnum = db.count_rows()
     with shelve.open(shelve_name) as storage:
     	storage['rows_count'] = rowsnum
-
-def get_rows_count():
-    """
-    Получает из хранилища количество строк в БД
-    :return: (int) Число строк
-    """
-    with shelve.open(shelve_name) as storage:
-        rowsnum = storage['rows_count']
     return rowsnum
+
+
 
