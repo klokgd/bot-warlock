ef49b8ca619476ecc73a59f88085b0d2dcfe31c4
mariupol <jokzik@gmail.com>
2018-09-16 20:11:57 +0300

    Код лучше.

--- config.py
+++ config.py
@@ -1,11 +1,2 @@
-token = ''
+token = '665443840:AAFT1oR5NT5RA5pWMK8mzVxohz07T8TVNWw'
 database_name = 'warlock.db'6
-shelve_name = 'shelve.db'
-REQUEST_KWARGS={
-    'proxy_url': 'http://socks5.svoi-an.us:1080/',
-    # Optional, if you need authentication:
-    'urllib3_proxy_kwargs': {
-        'username': 'privet',
-        'password': 'roskomnadzor',
-    }
-}
